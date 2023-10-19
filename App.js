ecf98c06c3e8201eace8791c75aee80c74f7a66d
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-10-19 08:18:58 -0300

    create login screen component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './components/loginScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Janaubers ta on</Text>
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
